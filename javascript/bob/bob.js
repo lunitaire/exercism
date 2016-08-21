6129471d8c21a3ad47abf933c33168db2f4ceb94
awagner <awagner@aptosolutions.com>
2016-08-20 23:42:36 -0400

    Wrote solution to Javascript bob exercise.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,15 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  if (input == "") {
+    return 'Fine. Be that way!';
+  } else if (input.substr(input.length - 1) == '?') {
+    return 'Sure.';
+  } else if (input == input.toUpperCase()) {
+    return 'Whoa, chill out!';
+  } else {
+    return 'Whatever.';
+  }
 };
 
 module.exports = Bob;
